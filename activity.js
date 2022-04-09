708ba667845d6920c1d98c48e8c54e8fe42c9375
Char Glen <cdg5be@virginia.edu>
2022-04-09 00:57:02 -0400

    activity solutions

--- activity.js
+++ activity.js
@@ -12,47 +12,295 @@ const bookList = [
     { title: "Ulysses", author: "James Joyce" },
     { title: "The Great Gatsby", author: "F. Scott Fitzgerald" },
     { title: "Moby Dick", author: "Herman Melville" },
-  ];
+];
 
-  /* Intermediate Challenge */
-  
-  /* (4) Write a one-line arrow function that takes in a number and returns a string stating whether the
-    number is positive or negative using a ternary operator (assume the number will never be zero) */
-  
-  /* (5) Write a switch statement for a 'day' variable that prints out something based off of what day of
-    the week it is
-    i.e. if it's Monday, print "good luck" or wednesday print "hump day" or friday print "party" */
-  
-  /* (6) Write an arrow function that takes in a number, and uses a for loop to return the sum of every
-    number from 1 up to that number
-    ex. sumUp(7) = 28 */
-  
-  /* Harder Challenge */
+/* 1 */
 
-  /* (7) Write an arrow function that converts the temperature from Celsius to Fahrenheit and then tells
+const addBook = (list, title, author) => {
+  const book = {
+    title: title,
+    author: author,
+  }
+
+  list.push(book)
+}
+
+/* 2 */
+
+const removeBook = (list, title) => {
+  list.forEach(b => {
+    if(b.title === title){
+        list.splice(list.indexOf(b), 1)
+    }
+  })
+}   
+
+/* 3 */
+
+const printBooks = (list) => {
+  list.forEach(book => {
+    console.log("Title: " + book.title)
+    console.log("Author: " + book.author) 
+  });
+}
+
+/* Intermediate Challenge */
+  
+/* (4) Write a one-line arrow function that takes in a number and returns a string stating whether the
+  number is positive or negative using a ternary operator (assume the number will never be zero) */
+
+const posOrNeg = (num) => {
+  return (num > 0 ? "positive" : "negative")
+}
+  
+/* (5) Write a switch statement for a 'day' variable that prints out something based off of what day of
+  the week it is i.e. if it's Monday, print "good luck" or wednesday print "hump day" or friday print "party" */
+
+const dailyMessage = (day) => {
+  if(typeof day !== "string"){
+    return "invalid day";
+  }
+  switch(day){
+    case day === "Monday":
+      console.log("good luck")
+      break;
+    case day === "Tuesday":
+      console.log("Taco Tuesday!")
+      break;
+    case day === "Wednesday":
+      console.log("hump day!!")
+      break;
+    case day === "Thursday":
+      console.log("Almost Friday")
+      break;
+    case day === "Friday":
+      console.log("party time")
+      break;
+    case day === "Saturday":
+      console.log("Who doesn't love Saturday")
+      break;
+    case day === "Sunday":
+      console.log("lazy Sunday")
+      break;
+  } 
+}
+  
+/* (6) Write an arrow function that takes in a number, and uses a for loop to return the sum of every
+  number from 1 up to that number
+  ex. sumUp(7) = 28 */
+
+const sumUpTo = (num) => {
+  let sum = 0
+    for (let i = 1; i <= num; i++){
+      sum += i
+    }
+  return sum
+}
+  
+/* Harder Challenge */
+
+/* (7) Write an arrow function that converts the temperature from Celsius to Fahrenheit and then tells
     me what I should wear accordingly */
-  
-  /* (8) Write a function that takes in an array and prints out the amount of truthy values in that array
-    using .forEach() */
-  
-  const exampleArray = ["Hello, world!", 8, null, false, "", "0", -22];
-  
-  /* (9) Using the map function and arrow syntax, return an array of object that contain a fullName field
-    and an averageGrade field representing the letter grade that corresponds to their GPA */
-  
-  const attendance = [
-    { firstName: "Clay", lastName: "Tondreau", gpa: 4.0 },
-    { firstName: "Tucker", lastName: "Wilson", gpa: 2.0 },
-    { firstName: "Eliza", lastName: "Tobin", gpa: 3.7 },
-    { firstName: "Sofia", lastName: "Ackerman", gpa: 1.1 },
-    { firstName: "Thomas", lastName: "Beddow", gpa: 2.3 },
-    { firstName: "Jackson", lastName: "Wolf", gpa: 4.0 },
-    { firstName: "Jared", lastName: "Nguyen", gpa: 4.0 },
-  ];
 
-  /* Hardest Challenge (Don't do this without completing harder challenges) */
+const whatToWear = (temp) => {
+  let fTemp = (1.8*temp) + 32
+  switch(fTemp){  
+    case fTemp >= 90:
+      console.log("Bathing suit!");
+      break;
+    case fTemp >= 80:
+      console.log("Shorts and t-shirt");
+      break;
+    case fTemp >= 70:
+      console.log("Pants and t-shirt"); 
+      break;
+    case fTemp >= 60:
+      console.log("Pants and long-sleeve shirt");
+      break;
+    case fTemp >= 50:
+      console.log("Pants and sweater");
+      break;
+    case fTemp >= 40:
+      console.log("Pants and jacket");
+      break;
+    case fTemp < 40:
+      console.log("Bundle Up!")
+      break;
+  }
+}
+  
+/* (8) Write a function that takes in an array and prints out the amount of truthy values in that array
+  using .forEach() */
+  
+const exampleArray = ["Hello, world!", 8, null, false, "", "0", -22];
+
+const numTruthy = (arr) => {
+  let num = 0;
+  arr.forEach(e => {
+    if(e){
+      num++;
+    }
+  })
+  return num;
+}
+  
+/* (9) Using the map function and arrow syntax, return an array of object that contain a fullName field
+  and an averageGrade field representing the letter grade that corresponds to their GPA */
+  
+const attendance = [
+  { firstName: "Clay", lastName: "Tondreau", gpa: 4.0 },
+  { firstName: "Tucker", lastName: "Wilson", gpa: 2.0 },
+  { firstName: "Eliza", lastName: "Tobin", gpa: 3.7 },
+  { firstName: "Sofia", lastName: "Ackerman", gpa: 1.1 },
+  { firstName: "Thomas", lastName: "Beddow", gpa: 2.3 },
+  { firstName: "Jackson", lastName: "Wolf", gpa: 4.0 },
+  { firstName: "Jared", lastName: "Nguyen", gpa: 4.0 },
+];
+
+const convertGrade = (grade) => { //sorry it's ugly!
+  if(grade >= 3.8)
+    return 'A'
+  else if(grade >= 3.5)
+    return 'A-'
+  else if(grade >= 3.3)
+    return 'B+'
+  else if(grade >= 3)
+    return 'B'
+  else if(grade >= 2.7)
+    return 'B-'
+  else if(grade >= 2.3)
+    return 'C+'
+  else if(grade >= 2)
+    return 'C'
+  else if(grade >= 1.7)
+    return 'C-'
+  else if(grade >= 1.3)
+    return 'D+'
+  else if(grade >= 1)
+    return 'D'
+  else if(grade >= .7)
+    return 'D-'
+  else  
+    return 'F'
+}
+
+const findLetterGrades = (list) => {
+  return list.map(e => {
+    const student = {
+      name: e.lastName + " " + e.firstName,
+      letter_grade: convertGrade(e.gpa),
+    }
+    return student
+  })
+}
+
+/* Hardest Challenge (Don't do this without completing harder challenges) */
 
   /* Write a function that solves the "every number eventually equals 4" puzzle. The output should be
     an array of the path you took to make it equal four
     ex/ [11, 6, 3, 5, 4], [19, 8, 5, 4] or [252, 18, 8, 5, 4]
     For context: https://puzzling.stackexchange.com/questions/29137/every-number-eventually-equals-4 */
+
+// assuming num < 1,000,000. Pattern holds with higher numbers but just requires more checks
+// does not support leading zeros
+
+const digToNumLetters = (num) => {
+  if(num === '6' || num === '1' || num === '2' || num === '10')
+    return 3
+  else if(num === '4' || num === '5' || num === '9'){
+    return 4    
+  }
+  else if(num === '8' || num === '3' || num === '7'){
+    return 5
+  }
+  else if(num === '11' || num === '12'){
+    return 6
+  }
+  else if(num === '15' || num === '16'){
+    return 7
+  }
+  else if(num === '13' || num === '14' || num === '18'  || num === '19'){
+    return 8
+  }
+  else if(num === '17'){
+    return 9
+  }
+  else
+    return 0
+}
+
+const tenToNumLetters = (dig) => { //for 20, 30, 40, etc
+  if(dig === '2' || dig === '3' || dig === '8' || dig === '9'){
+    return 6
+  }
+  else if(dig === '4' || dig === '5' || dig === '6'){
+    return 5
+  }
+  else if(dig === '7'){
+    return 7
+  }
+  else if(dig === '1'){
+    return 3
+  }
+  else if(dig === '0'){
+    return 0
+  }
+}
+
+const eventuallyFour = (num, path) => {
+  
+  if(num === 4){
+    path.push(num)
+    return path
+  }
+
+  const n = num.toString()
+  const len = n.length 
+  let newNum = 0
+  let i = len-1
+
+  while(n.charAt(i) !== ''){
+    
+    if(i === len-1){
+      if(len > 1){
+        if(n[len-2]==='1'){
+          newNum += digToNumLetters(n.substring(len-2, len))
+        }
+        else{
+          newNum += digToNumLetters(n[len-1]) + tenToNumLetters(n[len-2])
+        }
+        i -= 2;
+        continue;
+      }
+      else{
+        newNum = digToNumLetters(n)
+      }
+    }
+
+    if(i===len-3 && n[len-3] !== '0'){
+      newNum += 7 + digToNumLetters(n[len-3])
+    }
+
+    if(i===len-4){
+      newNum += 8 + digToNumLetters(n[len-4])
+    }
+
+    if(i===len-5){
+      newNum += tenToNumLetters(n[len-5])
+    }
+
+    if(i===len-6){
+      newNum += 7 + digToNumLetters(n[len-6])
+    }
+
+    i--;
+  }  
+  path.push(num)
+  path = eventuallyFour(newNum, path) //recursion!
+  return path
+}
+
+// need to call the function with an empty array 
+// eg: console.log(eventuallyFour(1234, []))
+
+console.log(eventuallyFour(11, []))
